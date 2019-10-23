51338f1b19487533a70772274b66f63500799117
papillon078 <ldc.morin@gmail.com>
2019-10-23 14:13:33 +0200

    exo 2 avant correction

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function hide(){
+document.getElementById('text').style.display = 'none';
+}
+function show(){
+document.getElementById('text').style.display = 'block';
+}
+//".style.display gere le mode d'affichage du div on le met sur "none pour l'effacer et sur block pour
+// faire reprendre son mode natif : block
